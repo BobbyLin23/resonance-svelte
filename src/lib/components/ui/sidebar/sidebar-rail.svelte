4fabb4946aa3dd5fe1402f3845d746de2479ee50
BobbyLin <linzhangsheng23@gmail.com>
2026-03-01 19:23:37 +0800

    chore: init project

---
+++ src/lib/components/ui/sidebar/sidebar-rail.svelte
@@ -0,0 +1,37 @@
+<script lang='ts'>
+  import type { WithElementRef } from '$lib/utils.js'
+  import type { HTMLAttributes } from 'svelte/elements'
+  import { cn } from '$lib/utils.js'
+  import { useSidebar } from './context.svelte.js'
+
+  let {
+    ref = $bindable(null),
+    class: className,
+    children,
+    ...restProps
+  }: WithElementRef<HTMLAttributes<HTMLButtonElement>, HTMLButtonElement> = $props()
+
+  const sidebar = useSidebar()
+</script>
+
+<button
+  bind:this={ref}
+  data-sidebar='rail'
+  data-slot='sidebar-rail'
+  aria-label='Toggle Sidebar'
+  tabIndex={-1}
+  onclick={sidebar.toggle}
+  title='Toggle Sidebar'
+  class={cn(
+    'hover:after:bg-sidebar-border absolute inset-y-0 z-20 hidden w-4 -translate-x-1/2 transition-all ease-linear group-data-[side=left]:-inset-e-4 group-data-[side=right]:inset-s-0 after:absolute after:inset-y-0 after:inset-s-[calc(1/2*100%-1px)] after:w-[2px] sm:flex',
+    'in-data-[side=left]:cursor-w-resize in-data-[side=right]:cursor-e-resize',
+    '[[data-side=left][data-state=collapsed]_&]:cursor-e-resize [[data-side=right][data-state=collapsed]_&]:cursor-w-resize',
+    'hover:group-data-[collapsible=offcanvas]:bg-sidebar group-data-[collapsible=offcanvas]:translate-x-0 group-data-[collapsible=offcanvas]:after:start-full',
+    '[[data-side=left][data-collapsible=offcanvas]_&]:-inset-e-2',
+    '[[data-side=right][data-collapsible=offcanvas]_&]:-inset-s-2',
+    className,
+  )}
+  {...restProps}
+>
+  {@render children?.()}
+</button>
